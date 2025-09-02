2adde31901ced4668ebf8cb2b2c2ecfd3f0d724f
João Neumann <161613347+neumannjoao@users.noreply.github.com>
2025-09-01 21:39:45 -0300

    .

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,8 +1,9 @@
 # app/hello/routes.py
-from flask import Blueprint
+from flask import Blueprint, render_template
 # Cria um objeto Blueprint. 'hello' é o nome do blueprint.
 hello_bp = Blueprint('hello', __name__)
 # Define uma rota dentro deste blueprint.
 @hello_bp.route('/')
 def index():
-    return "Hello, World!"
+    usuarios = ['André', 'Rafael', 'Fernando']
+    return render_template ('index.html', usuarios = usuarios)
