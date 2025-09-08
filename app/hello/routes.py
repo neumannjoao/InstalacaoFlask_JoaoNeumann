5ed6b863a46a148dcc503e43c49b2bae01de4efa
João Neumann <161613347+neumannjoao@users.noreply.github.com>
2025-09-08 20:22:37 -0300

    funcionando

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,5 +1,5 @@
 # app/hello/routes.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from ..models import User, db
 # Cria um objeto Blueprint. 'hello' é o nome do blueprint.
 hello_bp = Blueprint('hello', __name__)
@@ -8,3 +8,13 @@ hello_bp = Blueprint('hello', __name__)
 def index():
     usuarios = User.query.all() #['André', 'Rafael', 'Fernando']
     return render_template ('index.html', usuarios = usuarios)
+
+@hello_bp.route('/novousuario', methods=['POST'])
+def novoUsuario():
+    nome_usuario = request.form('nome_usuario')
+
+    novo_usuario= User(username = nome_usuario)
+
+    db.session.add(novo_usuario)
+    db.session.commit()
+    return redirect('/')
