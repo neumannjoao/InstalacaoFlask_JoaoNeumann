3d053cb6b9d055dcaf6a93fa9610475fd153c8b8
João Neumann <161613347+neumannjoao@users.noreply.github.com>
2025-09-08 21:43:39 -0300

    ok

--- app/hello/routes.py
+++ app/hello/routes.py
@@ -1,5 +1,5 @@
 # app/hello/routes.py
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..models import User, db
 # Cria um objeto Blueprint. 'hello' é o nome do blueprint.
 hello_bp = Blueprint('hello', __name__)
@@ -11,10 +11,26 @@ def index():
 
 @hello_bp.route('/novousuario', methods=['POST'])
 def novoUsuario():
-    nome_usuario = request.form('nome_usuario')
+    nome_usuario = request.form['nome_usuario']
 
     novo_usuario= User(username = nome_usuario)
 
     db.session.add(novo_usuario)
     db.session.commit()
     return redirect('/')
+
+@hello_bp.route('/removerUsuario/<int:usuario_id>', methods=['POST'])
+def removerUsuario(usuario_id):
+    usuario = User.query.get(usuario_id)
+    if usuario:
+        db.session.delete(usuario)
+        db.session.commit()
+    return redirect(url_for('hello.index'))
+
+@hello_bp.route('/editarUsuario/<int:usuario_id>', methods=["POST"])
+def editarUsuario(usuario_id):
+    usuario =User.query.get(usuario_id)
+    if usuario:
+        usuario.username = request.form['nome_usuario']
+        db.session.commit()
+    return redirect(url_for('hello.index'))
